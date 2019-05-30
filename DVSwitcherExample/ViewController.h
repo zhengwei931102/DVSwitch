722fee517205d650d00278689de7af4a643ad49e
精神薇 <zhengwei931102@163.com>
2019-05-30 22:02:31 +0800

    Initial commit

---
+++ DVSwitcherExample/ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  DVSwitcherExample
+//
+//  Created by Dmitry Volevodz on 08.10.14.
+//  Copyright (c) 2014 Dmitry Volevodz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DVSwitch;
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) DVSwitch *switcher;
+
+@end
+
+
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com 
